4f96d45c1ad8847bd1ead1af3704171cfbf0ba27
Shamim Hossain <hossains159@gmail.com>
2020-04-27 08:19:33 +0600

    add all the necessary setup for authors resource

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,12 +9,15 @@
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
-                    You are logged in!
+                    <passport-clients></passport-clients>
+                    <passport-authorized-clients></passport-authorized-clients>
+                    <passport-personal-access-tokens>
+                        </passport-personal-accesstokens>
                 </div>
             </div>
         </div>
