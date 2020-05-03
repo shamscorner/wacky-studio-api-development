160af76cebcc602aca1369fb45964cf865fd9de0
Shamim Hossain <hossains159@gmail.com>
2020-05-03 15:36:26 +0600

    add test for all authors endpoint

--- tests/Feature/AuthorsTest.php
+++ tests/Feature/AuthorsTest.php
@@ -15,7 +15,6 @@ class AuthorsTest extends TestCase
 
     /**
      * @test
-     * @watch
      */
     public function it_returns_an_author_as_a_resource_object()
     {
@@ -39,6 +38,52 @@ class AuthorsTest extends TestCase
                         'created_at' => $author->created_at->toJSON(),
                         'updated_at' => $author->updated_at->toJSON(),
                     ]
+                ]
+            ]);
+    }
+
+    /**
+     * @test
+     * @watch
+     */
+    public function It_returns_all_authors_as_a_collection_of_resource_objects()
+    {
+        $user = factory(User::class)->create();
+        Passport::actingAs($user);
+
+        $authors = factory(Author::class, 3)->create();
+
+        $this->get('/api/v1/authors')
+            ->assertStatus(200)
+            ->assertJson([
+                "data" => [
+                    [
+                        "id" => '1',
+                        "type" => "authors",
+                        "attributes" => [
+                            'name' => $authors[0]->name,
+                            'created_at' => $authors[0]->created_at->toJSON(),
+                            'updated_at' => $authors[0]->updated_at->toJSON(),
+                        ]
+                    ],
+                    [
+                        "id" => '2',
+                        "type" => "authors",
+                        "attributes" => [
+                            'name' => $authors[1]->name,
+                            'created_at' => $authors[1]->created_at->toJSON(),
+                            'updated_at' => $authors[1]->updated_at->toJSON(),
+                        ]
+                    ],
+                    [
+                        "id" => '3',
+                        "type" => "authors",
+                        "attributes" => [
+                            'name' => $authors[2]->name,
+                            'created_at' => $authors[2]->created_at->toJSON(),
+                            'updated_at' => $authors[2]->updated_at->toJSON(),
+                        ]
+                    ],
                 ]
             ]);
     }
